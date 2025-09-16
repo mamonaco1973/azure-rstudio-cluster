5be59ec569eecc8dc7c12d98e1bdfc65a9fecc2b
Mike Monaco <mamonaco1973@gmail.com>
2025-09-16 11:50:10 -0400

    add app-gateway subnet

--- 01-directory/network.tf
+++ 01-directory/network.tf
@@ -47,6 +47,17 @@ resource "azurerm_subnet" "bastion_subnet" {
 }
 
 # --------------------------------------------------------------------------------------------------
+# Define App Gateway Subnet (10.0.1.128/25)
+# NOTE: Application Gateway requires its own dedicated subnet
+# --------------------------------------------------------------------------------------------------
+resource "azurerm_subnet" "app_gateway_subnet" {
+  name                 = "app-gateway-subnet"
+  resource_group_name  = azurerm_resource_group.ad.name
+  virtual_network_name = azurerm_virtual_network.ad_vnet.name
+  address_prefixes     = ["10.0.1.128/25"]
+}
+
+# --------------------------------------------------------------------------------------------------
 # Define Network Security Group (NSG) for VM subnet
 # --------------------------------------------------------------------------------------------------
 resource "azurerm_network_security_group" "vm_nsg" {
@@ -113,4 +124,46 @@ resource "azurerm_network_security_group" "vm_nsg" {
 resource "azurerm_subnet_network_security_group_association" "vm_nsg_assoc" {
   subnet_id                 = azurerm_subnet.vm_subnet.id
   network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+# --------------------------------------------------------------------------------------------------
+# Define a network security group for the application gateway subnet
+# --------------------------------------------------------------------------------------------------
+
+resource "azurerm_network_security_group" "rstudio_gateway_nsg" {
+  name                = "rstudio-gateway-nsg"                         # Name of the NSG
+  location            = azurerm_resource_group.ad.location
+  resource_group_name = azurerm_resource_group.ad.name
+
+  security_rule {
+    name                       = "Allow-HTTP"                         # Rule name: Allow HTTP traffic
+    priority                   = 1002                                 # Rule priority
+    direction                  = "Inbound"                            # Traffic direction
+    access                     = "Allow"                              # Allow or deny rule
+    protocol                   = "Tcp"                                # Protocol type
+    source_port_range          = "*"                                  # Source port range
+    destination_port_range     = "80"                                 # Destination port
+    source_address_prefix      = "*"                                  # Source address range
+    destination_address_prefix = "*"                                  # Destination address range
+  }
+
+  security_rule {
+    name                       = "Allow-AppGateway-Ports"             # Rule name: Allow App Gateway ports
+    priority                   = 1003                                 # Rule priority
+    direction                  = "Inbound"                            # Traffic direction
+    access                     = "Allow"                              # Allow or deny rule
+    protocol                   = "Tcp"                                # Protocol type
+    source_port_range          = "*"                                  # Source port range
+    destination_port_ranges    = ["65200-65535"]                      # Destination port range
+    source_address_prefix      = "*"                                  # Source address range
+    destination_address_prefix = "*"                                  # Destination address range
+  }
+}
+
+# --------------------------------------------------------------------------------------------------
+# Associate NSG with App Gateway subnet
+# --------------------------------------------------------------------------------------------------
+resource "azurerm_subnet_network_security_group_association" "app_gateway_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.app_gateway_subnet.id
+  network_security_group_id = azurerm_network_security_group.rstudio_gateway_nsg.id
 }
