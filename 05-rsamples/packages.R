40e061f86ac3337409b605ff57202257d08658f0
Mike Monaco <mamonaco1973@gmail.com>
2025-09-17 19:25:49 -0400

    restructure resource groups

--- 05-rsamples/packages.R
+++ 05-rsamples/packages.R
@@ -1,14 +1,14 @@
-# Example: install gsl into /efs/rlibs
+# Example: install gsl into /nfs/rlibs
 
-install.packages("gsl", lib = "/efs/rlibs")
+install.packages("gsl", lib = "/nfs/rlibs")
 
 # Example: install gsl into personal library
 
 # install.packages("gsl")
 
-# Example: install ggplot2 into /efs/rlibs
+# Example: install ggplot2 into /nfs/rlibs
 
-# install.packages("ggplot2", lib = "/efs/rlibs")
+# install.packages("ggplot2", lib = "/nfs/rlibs")
 
 # Example: install ggplot2 into personal library
 
