9c477ad2e42e0f68a73acb019c836afe2647e3bd
Mike Monaco <mamonaco1973@gmail.com>
2025-09-19 13:13:16 -0400

    remove bastion nsg

--- 01-directory/bastion.tf
+++ 01-directory/bastion.tf
@@ -6,67 +6,6 @@
 # ==================================================================================================
 
 # --------------------------------------------------------------------------------------------------
-# Define a Network Security Group (NSG) for the Bastion host
-# --------------------------------------------------------------------------------------------------
-resource "azurerm_network_security_group" "bastion_nsg" {
-  name                = "bastion-nsg"
-  location            = azurerm_resource_group.ad.location
-  resource_group_name = azurerm_resource_group.ad.name
-
-  # Allow inbound traffic from Azure Gateway Manager (required for Bastion service)
-  security_rule {
-    name                       = "GatewayManager"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "GatewayManager"
-    destination_address_prefix = "*"
-  }
-
-  # Allow inbound HTTPS traffic from Internet (used to connect via Bastion public IP)
-  security_rule {
-    name                       = "Internet-Bastion-PublicIP"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  # Allow outbound RDP (3389) and SSH (22) to internal virtual network
-  security_rule {
-    name                       = "OutboundVirtualNetwork"
-    priority                   = 1001
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["22", "3389"]
-    source_address_prefix      = "*"
-    destination_address_prefix = "VirtualNetwork"
-  }
-
-  # Allow outbound HTTPS to Azure Cloud (required for Bastion management/control plane)
-  security_rule {
-    name                       = "OutboundToAzureCloud"
-    priority                   = 1002
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "*"
-    destination_address_prefix = "AzureCloud"
-  }
-}
-
-# --------------------------------------------------------------------------------------------------
 # Allocate a Public IP for the Bastion host
 # --------------------------------------------------------------------------------------------------
 resource "azurerm_public_ip" "bastion_ip" {
