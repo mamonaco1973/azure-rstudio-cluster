9dc9ab63de35fda1d0b1a0de208f55b6ec453adf
Mike Monaco <mamonaco1973@gmail.com>
2026-05-25 10:00:22 -0400

    clean up custom_data logging

--- 02-servers/scripts/custom_data.sh
+++ 02-servers/scripts/custom_data.sh
@@ -252,6 +252,7 @@ cd /nfs
 git clone https://github.com/mamonaco1973/azure-rstudio-cluster.git || true
 chmod -R 775 azure-rstudio-cluster || true
 chgrp -R rstudio-users azure-rstudio-cluster || true
+mkdir -p /home/ubuntu
 chown -R ubuntu:ubuntu /home/ubuntu || true
 echo "NOTE: [homedir] done"
 
